f9ff7c4e208eff4c0ebc837fec450a58bad8133e
[sulfidate] <[ffluid@me.com]>
2022-10-02 23:50:59 +0200

    HomeScreen Product List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
@@ -10,13 +10,12 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-      <h1>Welcome To ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
-
     </>
-  );
+  )
 }
 
-export default App;
+export default App

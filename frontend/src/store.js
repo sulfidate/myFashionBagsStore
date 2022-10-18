4114b453a12c73fa69b860227c47dfd631dd50fd
[sulfidate] <[ffluid@me.com]>
2022-10-18 21:48:43 +0200

    Product List Reducer & Action, Bringing Redux State into HomeScreen, useDispatch & useSelector

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducers/productReducers'
 
 const reducer = combineReducers({
   // <-- add your reducers here
+  productList: productListReducer,
 })
 
 const initialState = {}
